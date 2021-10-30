abd86dae24be9755b52ddada27f85ebe603180bb
Anastasia <anastasiaberia00@gmail.com>
2021-10-30 20:49:13 +0300

    fixed sh file

--- .github/scripts/add-release-yt.sh
+++ .github/scripts/add-release-yt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cur_tag=$(git tag | tail -1 | head -n1)
-previous_tag=$(git tag | tail -2 | head -n1)
-cur_tag_author=$(git show $cur_tag --no-patch)
-cur_tag_date=$(git show $cur_tag --no-patch)
+cur_tag=$(git tag | sort -r | head -1)
+previous_tag=$(git tag | sort -r | head -2 | tail -1)
+cur_tag_author=$(git show ${cur_tag} | grep Author: )
+cur_tag_date=$(git show ${cur_tag} | grep Date:)
 log=`git log $previous_tag`
 
 summary="Test issue ${cur_tag}"
