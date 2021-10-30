16da3f44fa610b51cefe0fe79bfe5cf56c0394a1
Anastasia <anastasiaberia00@gmail.com>
2021-10-30 22:35:32 +0300

    fixed header

--- scripts/add-release-yt.sh
+++ scripts/add-release-yt.sh
@@ -11,11 +11,14 @@ description="${cur_tag_author}:${cur_tag_date}:${cur_tag}"
 taskURL="https://api.tracker.yandex.net/v2/issues/"
 
 responseStatus=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request POST ${taskURL} \
---header "Authorization: OAuth ${OAuth}" \
---header "X-Org-ID: ${OrganizationId}" \
+--header "Authorization: ${OAuth}" \
+--header "X-Org-Id: ${OrganizationId}" \
 --header "Content-Type: application/json" \
 --data-raw '{
-    Some text
+    "queue": "TMP",
+    "summary": "'"${summary}"'",
+    "type": "task",
+    "description": "'"${description}"'"
 }')
 
 
