45fd6fc3630f17547b7a4d3c2c500f1089131e09
Anastasia <anastasiaberia00@gmail.com>
2021-10-30 21:30:39 +0300

    truying bash not sh

--- .github/scripts/add-release-yt.sh
+++ .github/scripts/add-release-yt.sh
@@ -10,8 +10,8 @@ desc="${cur_tag_author}:${cur_tag_date}:${cur_tag}"
 taskURL="https://api.tracker.yandex.net/v2/issues/"
 
 responseStatus=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request POST ${taskURL} \
---header "Authorization: OAuth ${env.OAuth}" \
---header "X-Org-Id: ${env.OrganizationId}" \
+--header "Authorization: OAuth ${OAuth}" \
+--header "X-Org-Id: ${OrganizationId}" \
 --header "Content-Type: application/json" \
 --data-raw '{
     "queue": "TMP",
