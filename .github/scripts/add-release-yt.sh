624b75c5faabc3cbeef82bc312c49ac9ef24d832
Anastasia <anastasiaberia00@gmail.com>
2021-10-30 20:37:16 +0300

    fixed tags notation

--- .github/scripts/add-release-yt.sh
+++ .github/scripts/add-release-yt.sh
@@ -4,9 +4,9 @@ cur_tag_author=$(git show $cur_tag --no-patch)
 cur_tag_date=$(git show $cur_tag --no-patch)
 log=`git log $previous_tag`
 
-summary="Test issue ${lastTag}"
+summary="Test issue ${cur_tag}"
 desc="${cur_tag_author}:${cur_tag_date}:${cur_tag}"
-unique= "Test estasie/${lastTag}"
+unique= "Test estasie ${cur_tag}"
 createTaskReqUrl="https://api.tracker.yandex.net/v2/issues/"
 
 responseStatus=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request POST ${createTaskReqUrl} \
