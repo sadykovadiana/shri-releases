d2cefb9cb253d59cefcfa14ad1945e8cf0c00836
Anastasia <anastasiaberia00@gmail.com>
2021-10-31 11:54:56 +0300

    Added comments at the task

--- scripts/add-release-yt.sh
+++ scripts/add-release-yt.sh
@@ -10,6 +10,7 @@ desc=$(git log --pretty=format:"%h - %s (%an, %ar)\n" | tr -s "\n")
 summary="$cur_tag: Estasie App update"
 description="${cur_tag_author}:${cur_tag_date}:${cur_tag}"
 taskURL="https://api.tracker.yandex.net/v2/issues/"
+taskID="Unique/estasie/$cur_tag"
 
 responseStatus=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request POST ${taskURL} \
 --header "Authorization: OAuth ${OAuth}" \
@@ -19,20 +20,48 @@ responseStatus=$(curl --write-out '%{http_code}' --silent --output /dev/null --l
     "queue": "TMP",
     "summary": "'"${summary}"'",
     "type": "task",
-    "description": "'"${desc}"'"
+    "description": "'"${desc}"'",
+    "taskID": "'"${taskID}"'"
 }')
-
-
-
-
-
- if [ "$responseStatus" -ne 201 ]
+if [ "$responseStatus" -ne 201 ]
     then
         echo "ERROR: ${responseStatus}"
-        exit 1
     else
         echo "Task added"
         echo "$cur_tag, $previous_tag"
-        exit 0
+fi
+
+getTaskId=$(curl --silent --location --request POST "https://api.tracker.yandex.net/v2/issues/_search" \
+--header "Authorization: OAuth ${OAuth}" \
+--header "X-Org-Id: ${OrganizationId}" \
+--header "Content-Type: application/json" \
+    --data-raw '{
+        "filter": {
+            "taskID": "'"${taskID}"'"
+        }
+    }' | jq -r '.[0].key'
+)
+
+
+if [ "$responseStatus" -eq 409 ]
+then
+    echo "Version duplicate, creating comment"
+    getCommentStatusRequest=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request PATCH \
+        "https://api.tracker.yandex.net/v2/issues/${getTaskId}" \
+        --header "${authHeader}" \
+        --header "${orgHeader}" \
+        --header "${contentType}" \
+        --data-raw '{
+            "summary": "'"${updatingSummary}"'",
+            "description": "'"${description}"'"
+        }'
+    )
+
+    if [ "$getCommentStatusRequest" -ne 200 ]
+    then
+        echo "ERROR: CANNOT UPDATE ${taskKey}"
+        exit 1
+    else
+        echo "SUCCSESS: ${taskKey} UPDATED"
     fi
 
